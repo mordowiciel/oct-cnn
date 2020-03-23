4672c065c1b89fd9f4c9d6c72525e75899164fed
mordowiciel <szymar05@gmail.com>
2020-03-23 01:29:57 +0100

    Slight modification of v0.3 - end the horizontal pool with square output and them perform one triple 3x3 CONV block with 'same' padding. Minimally increase number of filters in last layers. ~36 mln parameters

--- src/architectures/factornet.py
+++ src/architectures/factornet.py
@@ -4,51 +4,30 @@ from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 def FactorNet(input_shape, classes):
     model = Sequential()
 
-    # Vertical pooling
-    model.add(Conv2D(filters=32, kernel_size=(17, 1), activation='relu', padding='same', input_shape=input_shape))
-    model.add(Conv2D(filters=32, kernel_size=(1, 17), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 2), padding='same'))
+    # Aggressive, vertical pooling
+    model.add(Conv2D(filters=64, kernel_size=(1, 11), strides=(1, 2), activation='relu', input_shape=input_shape, name='conv_vertical_1'))
+    model.add(Conv2D(filters=64, kernel_size=(1, 11), strides=(1, 2), activation='relu', padding='valid', name='conv_vertical_2'))
+    model.add(Conv2D(filters=64, kernel_size=(1, 11), strides=(1, 2), activation='relu', padding='valid', name='conv_vertical_3'))
 
-    model.add(Conv2D(filters=32, kernel_size=(11, 1), activation='relu', padding='same'))
-    model.add(Conv2D(filters=32, kernel_size=(1, 11), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 2), padding='same'))
+    # Slower, horizontal pooling
+    model.add(Conv2D(filters=128, kernel_size=(17, 1), activation='relu', padding='valid', name='conv_horizontal_1'))
+    model.add(Conv2D(filters=128, kernel_size=(17, 1), activation='relu', padding='valid', name='conv_horizontal_2'))
+    model.add(Conv2D(filters=128, kernel_size=(17, 1), activation='relu', padding='valid', name='conv_horizontal_3'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 1), padding='valid', name='maxpool_horizontal_3'))
 
-    model.add(Conv2D(filters=32, kernel_size=(7, 1), activation='relu', padding='same'))
-    model.add(Conv2D(filters=32, kernel_size=(1, 7), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 2), padding='same'))
+    model.add(Conv2D(filters=192, kernel_size=(11, 1), activation='relu', padding='valid', name='conv_horizontal_4'))
+    model.add(Conv2D(filters=192, kernel_size=(11, 1), activation='relu', padding='valid', name='conv_horizontal_5'))
+    model.add(Conv2D(filters=192, kernel_size=(11, 1), activation='relu', padding='valid', name='conv_horizontal_6'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 1), padding='valid', name='maxpool_horizontal_6'))
 
-    # Horizontal pooling
-    model.add(Conv2D(filters=64, kernel_size=(11, 1), activation='relu', padding='same'))
-    model.add(Conv2D(filters=64, kernel_size=(1, 11), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 1), padding='same'))
+    model.add(Conv2D(filters=192, kernel_size=(7, 1), activation='relu', padding='valid', name='conv_horizontal_7'))
+    model.add(Conv2D(filters=192, kernel_size=(7, 1), activation='relu', padding='valid', name='conv_horizontal_8'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='valid', name='maxpool_horizontal_8'))
 
-    model.add(Conv2D(filters=64, kernel_size=(7, 1), activation='relu', padding='same'))
-    model.add(Conv2D(filters=64, kernel_size=(1, 7), activation='relu', padding='same'))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 1), padding='same'))
-
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_1'))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                 name='conv_3x3_2'))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                 name='conv_3x3_3'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name='maxpool_3x3_3'))
-
-    model.add(Conv2D(filters=196, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_4'))
-    model.add(Conv2D(filters=196, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_5'))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_6'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name='maxpool_3x3_6'))
-
-    model.add(Conv2D(filters=196, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_7'))
-    model.add(Conv2D(filters=196, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_8'))
-    model.add(Conv2D(filters=128, kernel_size=(3, 3), strides=(1, 1), padding='same', activation='relu',
-                     name='conv_3x3_9'))
-    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same', name='maxpool_3x3_9'))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same'))
+    model.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same'))
 
     # Fully connected layers
     model.add(Flatten())
